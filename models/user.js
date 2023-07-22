bce909c470f3303a5c62edd5e39013e4d55cfeeb
Mohamed Fazil M <fazilmm860@gmail.com>
2023-07-22 23:13:01 +0530

    completed the model with validate function in it using joi and joi-password-complexity

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity')
 
 const userSchema = mongoose.Schema({
     fistName: {
@@ -21,5 +24,20 @@ const userSchema = mongoose.Schema({
 })
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY)
+    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, { expiresIn: "7d" })
+    return token
 }
+
+const User = mongoose.model("User", userSchema)
+
+const validate = (data) => {
+    const schema = Joi.object({
+        firstName: Joi.string().required().label("First Name"),
+        lastName: Joi.string().required().label("Last Name"),
+        email: Joi.string().email().required().label("Email"),
+        password: passwordComplexity().required().label("Password")
+    });
+    return schema.validate(data)
+}
+
+module.exports = { User, validate }
