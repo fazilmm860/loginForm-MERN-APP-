f35e627d080f40023f3c05e3764ba2183994ad0a
Mohamed Fazil M <fazilmm860@gmail.com>
2023-07-24 18:56:26 +0530

    Login component created

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
         await new User({ ...req.body, password: hashPassword }).save()
         res.status(201).send({ message: "User Created" })
     } catch (error) {
-        res.status(500).send({ message: "Internal Server Error" })
+        res.status(500).send({ message: "Internal Server Error", error })
     }
 })
 
